59808a06964eb837ce7d2f974b9c3e1dd53867e9
czenturion <czenturijon@gmail.com>
2023-01-15 15:05:55 +0300

    v0.98 | gsn deployed with gh-pages

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const mapStateToProps = (state) => ({
 const AppContainer = connect(mapStateToProps, {initializeApp})(App)
 
 const MainApp = (props) => {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
