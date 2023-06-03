d275352d4e3b03d68082ef8c5432c3876e5fac0d
czenturion <czenturijon@gmail.com>
2023-06-03 14:17:05 +0300

    v1.1.6 | transfer on antd layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import Navbar from "./components/Navbar/Navbar"
-import {HashRouter, Navigate, Route, Routes} from "react-router-dom"
+import {HashRouter, Navigate, NavLink, Route, Routes} from "react-router-dom"
 import UsersContainer from "./components/Users/UsersContainer"
 import ProfileContainer from "./components/Profile/ProfileContainer"
 import HeaderContainer from "./components/Header/HeaderContainer"
@@ -10,6 +10,11 @@ import {connect, Provider} from "react-redux"
 import {initializeApp} from "./redux/app-reducer"
 import Preloader from "./components/common/Preloader/Preloader"
 import store from "./redux/redux-store"
+import "antd/dist/reset.css"
+import { Breadcrumb, Layout, Menu, SubMenu, theme } from "antd"
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from "@ant-design/icons"
+
+const { Header, Content, Sider } = Layout
 
 const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'))
 const News = lazy(() => import('./components/News/News'))
@@ -18,8 +23,13 @@ const Settings = lazy(() => import('./components/Settings/Settings'))
 const Login = lazy(() => import('./components/Login/Login'))
 const NotFound = lazy(() => import('./components/NotFound/NotFound'))
 
+const items1 = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
 
 class App extends Component {
+
     catchAllUnhandledErrors = (promise) => {
         alert(promise.reason.response.data.message)
     }
@@ -38,26 +48,92 @@ class App extends Component {
             return <Preloader/>
         } else {
             return (
-                <div className="app-wrapper">
-                    <HeaderContainer/>
-                    <Navbar/>
-                    <div className="app-wrapper-content">
-                        <Suspense fallback={<Preloader/>}>
-                            <Routes>
-                                <Route path="/" element={<Navigate to={"/profile"}/>}/>
-                                <Route path="/profile/:userId" element={<ProfileContainer/>}/>
-                                <Route path="/profile" element={<ProfileContainer/>}/>
-                                <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                                <Route path="/news" element={<News/>}/>
-                                <Route path="/music" element={<Music/>}/>
-                                <Route path="/settings" element={<Settings/>}/>
-                                <Route path="/users" element={<UsersContainer/>}/>
-                                <Route path="/login" element={<Login/>}/>
-                                <Route path="*" element={<NotFound/>}/>
-                            </Routes>
-                        </Suspense>
-                    </div>
-                </div>
+                <Layout>
+                    <Header style={{ display: 'flex', alignItems: 'center' }}>
+                        <div className="demo-logo" />
+                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+                    </Header>
+                    <Layout>
+                        <Sider width={200} style={{ background: 'white' }}>
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{ height: '100%', borderRight: 0 }}
+                            >
+                                <Menu.SubMenu key="sub1" title="My Profile">
+                                    <Menu.Item key="1">
+                                        <NavLink to="/profile" style={({isActive}) => ({
+                                            color: isActive ? 'blue' : 'black'
+                                        })}>Profile</NavLink>
+                                    </Menu.Item>
+                                    <Menu.Item key="2">
+                                        <NavLink to="/dialogs" style={({isActive}) => ({
+                                            color: isActive ? 'blue' : 'black'
+                                        })}>Messages</NavLink>
+                                    </Menu.Item>
+                                </Menu.SubMenu>
+                                <Menu.SubMenu key="sub2" title="Developers">
+                                    <Menu.Item className="Item">
+                                        <NavLink to="/users" style={({isActive}) => ({
+                                            color: isActive ? 'blue' : 'black'
+                                        })}>Find users</NavLink>
+                                    </Menu.Item>
+                                </Menu.SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Layout style={{ padding: '0 24px 24px' }}>
+                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                <Breadcrumb.Item>List</Breadcrumb.Item>
+                                <Breadcrumb.Item>App</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <Content
+                                style={{
+                                    padding: 24,
+                                    margin: 0,
+                                    minHeight: 280,
+                                    background: 'white',
+                                }}
+                            >
+                                <Suspense fallback={<Preloader/>}>
+                                    <Routes>
+                                        <Route path="/" element={<Navigate to={"/profile"}/>}/>
+                                        <Route path="/profile/:userId" element={<ProfileContainer/>}/>
+                                        <Route path="/profile" element={<ProfileContainer/>}/>
+                                        <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                                        <Route path="/news" element={<News/>}/>
+                                        <Route path="/music" element={<Music/>}/>
+                                        <Route path="/settings" element={<Settings/>}/>
+                                        <Route path="/users" element={<UsersContainer/>}/>
+                                        <Route path="/login" element={<Login/>}/>
+                                        <Route path="*" element={<NotFound/>}/>
+                                    </Routes>
+                                </Suspense>
+                            </Content>
+                        </Layout>
+                    </Layout>
+                </Layout>
+                // <div className="app-wrapper">
+                //     <HeaderContainer/>
+                //     <Navbar/>
+                //     <div className="app-wrapper-content">
+                //         <Suspense fallback={<Preloader/>}>
+                //             <Routes>
+                //                 <Route path="/" element={<Navigate to={"/profile"}/>}/>
+                //                 <Route path="/profile/:userId" element={<ProfileContainer/>}/>
+                //                 <Route path="/profile" element={<ProfileContainer/>}/>
+                //                 <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                //                 <Route path="/news" element={<News/>}/>
+                //                 <Route path="/music" element={<Music/>}/>
+                //                 <Route path="/settings" element={<Settings/>}/>
+                //                 <Route path="/users" element={<UsersContainer/>}/>
+                //                 <Route path="/login" element={<Login/>}/>
+                //                 <Route path="*" element={<NotFound/>}/>
+                //             </Routes>
+                //          </Suspense>
+                //     </div>
+                // </div>
             )
         }
     }
