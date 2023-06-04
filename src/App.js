fc5f4469b6c99ea3bf16516954b6545135406d93
czenturion <czenturijon@gmail.com>
2023-06-04 15:24:17 +0300

    v1.1.7 | transfer on antd layout 1/2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css"
 import Navbar from "./components/Navbar/Navbar"
-import {HashRouter, Navigate, NavLink, Route, Routes} from "react-router-dom"
+import { HashRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
 import UsersContainer from "./components/Users/UsersContainer"
 import ProfileContainer from "./components/Profile/ProfileContainer"
 import HeaderContainer from "./components/Header/HeaderContainer"
 import * as React from "react"
-import {Component, lazy, Suspense} from "react"
-import {connect, Provider} from "react-redux"
-import {initializeApp} from "./redux/app-reducer"
+import { Component, lazy, Suspense, useEffect, useState } from "react"
+import { connect, Provider } from "react-redux"
+import { initializeApp } from "./redux/app-reducer"
 import Preloader from "./components/common/Preloader/Preloader"
 import store from "./redux/redux-store"
 import "antd/dist/reset.css"
 import { Breadcrumb, Layout, Menu, SubMenu, theme } from "antd"
+import { logOut } from "./redux/auth-reducer"
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from "@ant-design/icons"
+import s from "./components/Header/Header.module.css"
 
 const { Header, Content, Sider } = Layout
 
@@ -23,128 +25,127 @@ const Settings = lazy(() => import('./components/Settings/Settings'))
 const Login = lazy(() => import('./components/Login/Login'))
 const NotFound = lazy(() => import('./components/NotFound/NotFound'))
 
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
 
-class App extends Component {
+const App = ({ initializeApp, initialized, login, logOut }) => {
 
-    catchAllUnhandledErrors = (promise) => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    const catchAllUnhandledErrors = ( promise ) => {
         alert(promise.reason.response.data.message)
     }
 
-    componentDidMount() {
-        this.props.initializeApp()
-        window.addEventListener("unhandledrejection", this.catchAllUnhandledErrors)
-    }
+    useEffect(() => {
+        initializeApp()
+        window.addEventListener("unhandledrejection", catchAllUnhandledErrors)
+        return () => {
+            window.removeEventListener("unhandledrejection", catchAllUnhandledErrors)
+        }
+    }, [] )
 
-    componentWillUnmount() {
-        window.removeEventListener("unhandledrejection", this.catchAllUnhandledErrors)
-    }
-
-    render() {
-        if (!this.props.initialized) {
-            return <Preloader/>
-        } else {
-            return (
+    if ( !initialized ) {
+        return <Preloader />
+    } else {
+        return (
+            <Layout>
+                <Header style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{ color: 'white' }}>
+                        { login }
+                    </div>
+                    <Menu theme="dark" mode="horizontal">
+                        <Menu.Item key="head1">
+                            <span onClick={ logOut }>Log Out</span>
+                        </Menu.Item>
+                    </Menu>
+                </Header>
                 <Layout>
-                    <Header style={{ display: 'flex', alignItems: 'center' }}>
-                        <div className="demo-logo" />
-                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
-                    </Header>
-                    <Layout>
-                        <Sider width={200} style={{ background: 'white' }}>
-                            <Menu
-                                mode="inline"
-                                defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
-                                style={{ height: '100%', borderRight: 0 }}
-                            >
-                                <Menu.SubMenu key="sub1" title="My Profile">
-                                    <Menu.Item key="1">
-                                        <NavLink to="/profile" style={({isActive}) => ({
-                                            color: isActive ? 'blue' : 'black'
-                                        })}>Profile</NavLink>
-                                    </Menu.Item>
-                                    <Menu.Item key="2">
-                                        <NavLink to="/dialogs" style={({isActive}) => ({
-                                            color: isActive ? 'blue' : 'black'
-                                        })}>Messages</NavLink>
-                                    </Menu.Item>
-                                </Menu.SubMenu>
-                                <Menu.SubMenu key="sub2" title="Developers">
-                                    <Menu.Item className="Item">
-                                        <NavLink to="/users" style={({isActive}) => ({
-                                            color: isActive ? 'blue' : 'black'
-                                        })}>Find users</NavLink>
-                                    </Menu.Item>
-                                </Menu.SubMenu>
-                            </Menu>
-                        </Sider>
-                        <Layout style={{ padding: '0 24px 24px' }}>
-                            <Breadcrumb style={{ margin: '16px 0' }}>
-                                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                                <Breadcrumb.Item>List</Breadcrumb.Item>
-                                <Breadcrumb.Item>App</Breadcrumb.Item>
-                            </Breadcrumb>
-                            <Content
-                                style={{
-                                    padding: 24,
-                                    margin: 0,
-                                    minHeight: 280,
-                                    background: 'white',
-                                }}
-                            >
-                                <Suspense fallback={<Preloader/>}>
-                                    <Routes>
-                                        <Route path="/" element={<Navigate to={"/profile"}/>}/>
-                                        <Route path="/profile/:userId" element={<ProfileContainer/>}/>
-                                        <Route path="/profile" element={<ProfileContainer/>}/>
-                                        <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                                        <Route path="/news" element={<News/>}/>
-                                        <Route path="/music" element={<Music/>}/>
-                                        <Route path="/settings" element={<Settings/>}/>
-                                        <Route path="/users" element={<UsersContainer/>}/>
-                                        <Route path="/login" element={<Login/>}/>
-                                        <Route path="*" element={<NotFound/>}/>
-                                    </Routes>
-                                </Suspense>
-                            </Content>
-                        </Layout>
+                    <Sider width={ 200 } style={{ background: colorBgContainer }}>
+                        <Menu
+                            mode="inline"
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
+                        >
+                            <Menu.SubMenu key="sub1" title="My Profile">
+                                <Menu.Item key="1">
+                                    <NavLink to="/profile" style={({ isActive }) => ({
+                                        color: isActive ? 'blue' : 'black'
+                                    })}><UserOutlined></UserOutlined> Profile</NavLink>
+                                </Menu.Item>
+                                <Menu.Item key="2">
+                                    <NavLink to="/dialogs" style={({ isActive }) => ({
+                                        color: isActive ? 'blue' : 'black'
+                                    })}>Messages</NavLink>
+                                </Menu.Item>
+                                <Menu.Item key="3">
+                                    <NavLink to="/music" style={({ isActive }) => ({
+                                        color: isActive ? 'blue' : 'black'
+                                    })}>Music</NavLink>
+                                </Menu.Item>
+                            </Menu.SubMenu>
+                            <Menu.SubMenu key="sub2" title="Developers">
+                                <Menu.Item key="4">
+                                    <NavLink to="/users" style={({ isActive }) => ({
+                                        color: isActive ? 'blue' : 'black'
+                                    })}>Find users</NavLink>
+                                </Menu.Item>
+                                <Menu.Item key="5">
+                                    <NavLink to="/settings" style={({ isActive }) => ({
+                                        color: isActive ? 'blue' : 'black'
+                                    })}>Settings</NavLink>
+                                </Menu.Item>
+                            </Menu.SubMenu>
+                            <Menu.Item key="6">
+                                <NavLink to="/news" style={({ isActive }) => ({
+                                    color: isActive ? 'blue' : 'black'
+                                })}>News</NavLink>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{padding: '0 24px 24px'}}>
+                        <Breadcrumb style={{margin: '16px 0'}}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Content
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 580,
+                                background: colorBgContainer,
+                            }}
+                        >
+                            <Suspense fallback={<Preloader/>}>
+                                <Routes>
+                                    <Route path="/" element={<Navigate to={"/profile"}/>}/>
+                                    <Route path="/profile/:userId" element={<ProfileContainer/>}/>
+                                    <Route path="/profile" element={<ProfileContainer/>}/>
+                                    <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                                    <Route path="/news" element={<News/>}/>
+                                    <Route path="/music" element={<Music/>}/>
+                                    <Route path="/settings" element={<Settings/>}/>
+                                    <Route path="/users" element={<UsersContainer/>}/>
+                                    <Route path="/login" element={<Login/>}/>
+                                    <Route path="*" element={<NotFound/>}/>
+                                </Routes>
+                            </Suspense>
+                        </Content>
                     </Layout>
                 </Layout>
-                // <div className="app-wrapper">
-                //     <HeaderContainer/>
-                //     <Navbar/>
-                //     <div className="app-wrapper-content">
-                //         <Suspense fallback={<Preloader/>}>
-                //             <Routes>
-                //                 <Route path="/" element={<Navigate to={"/profile"}/>}/>
-                //                 <Route path="/profile/:userId" element={<ProfileContainer/>}/>
-                //                 <Route path="/profile" element={<ProfileContainer/>}/>
-                //                 <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                //                 <Route path="/news" element={<News/>}/>
-                //                 <Route path="/music" element={<Music/>}/>
-                //                 <Route path="/settings" element={<Settings/>}/>
-                //                 <Route path="/users" element={<UsersContainer/>}/>
-                //                 <Route path="/login" element={<Login/>}/>
-                //                 <Route path="*" element={<NotFound/>}/>
-                //             </Routes>
-                //          </Suspense>
-                //     </div>
-                // </div>
-            )
-        }
+            </Layout>
+        )
     }
 }
 
+
 const mapStateToProps = (state) => ({
     initialized: state.app.initialized,
-    authIsFetching: state.auth.isFetching
+    authIsFetching: state.auth.isFetching,
+    login: state.auth.login
 })
 
-const AppContainer = connect(mapStateToProps, {initializeApp})(App)
+const AppContainer = connect(mapStateToProps, {initializeApp, logOut})(App)
 
 const MainApp = () => {
     return <HashRouter basename="/">
