f1a9f8d6917419357a951009fa9865760ee5b39f
czntrn <czenturijon@gmail.com>
2023-08-04 16:55:36 +0300

    dialogs bottuns updating

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,6 +1,7 @@
 import s from "./../Dialogs.module.css"
 import {NavLink} from "react-router-dom"
 import * as React from "react"
+import {Card} from "antd";
 
 type PropsType = {
     ava: string
@@ -11,10 +12,25 @@ type PropsType = {
 
 const DialogItem: React.FC<PropsType> = (props) => {
     return (
-        <div className={s.dialogElement}>
-            <NavLink to={"/dialogs/" + props.id} style={({isActive}) => ({
+        <div
+            className={s.dialogElement}
+        >
+            <NavLink
+                to={"/dialogs/" + props.id}
+                style={({isActive}) => ({
                 color: isActive ? '#66d801' : 'black'
-            })}><img src={props.ava} alt="ava"/><div className={s.text}>{props.name}</div></NavLink>
+            })}>
+                <Card>
+                    <img
+                        src={props.ava}
+                        alt="ava"
+                    />
+                    <div
+                        className={s.text}>
+                        {props.name}
+                    </div>
+                </Card>
+            </NavLink>
         </div>
     )
 }
